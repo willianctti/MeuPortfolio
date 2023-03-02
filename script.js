b0af5240e4558b27527dd7073ae92ca7bff10a12
willianctti <willianctti@gmail.com>
2023-03-02 17:01:42 -0300

    meus projetos

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let hill1 = document.querySelector('hill1')
 let hill4 = document.querySelector('#hill4')
 let hill5 = document.querySelector('#hill5')
 
+
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
@@ -11,4 +12,17 @@ window.addEventListener('scroll', () => {
     hill5.style.top = value * -1.2 + 'px'
     hill5.style.left = value * 7 + 'px'
     hill4.style.left = value * -1.5 + 'px'
+    
+    if (value > 200 ) {
+        text.style.display = "none";
+    } else {
+        text.style.display = "block";
+    }
 })
+
+
+
+   
+
+
+ 
